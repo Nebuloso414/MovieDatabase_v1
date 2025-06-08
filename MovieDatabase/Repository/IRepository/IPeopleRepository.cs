898ebc0dbf777fd0dc23e5e23e6cf5cd87a590e7
Guillermo Garcia <g2beastie@live.com>
2025-06-08 14:17:41 -0400

    Added People repository

---
+++ MovieDatabase/Repository/IRepository/IPeopleRepository.cs
@@ -0,0 +1,10 @@
+﻿using MovieDatabase.Models;
+
+namespace MovieDatabase.Repository.IRepository
+{
+    public interface IPeopleRepository :IBaseRepository<People>
+    {
+        Task<IEnumerable<People>> GetAllWithDetailsAsync();
+        Task<IEnumerable<People?>> GetByNameAsync(string name);
+    }
+}
---
+++ MovieDatabase/Repository/PeopleRepository.cs
@@ -0,0 +1,22 @@
+﻿using MovieDatabase.Data;
+using MovieDatabase.Models;
+using MovieDatabase.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MovieDatabase.Repository
+{
+    public class PeopleRepository : BaseRepository<People>, IPeopleRepository
+    {
+        public PeopleRepository(ApplicationDbContext context) : base(context) { }
+        
+        public Task<IEnumerable<People>> GetAllWithDetailsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<People?>> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
