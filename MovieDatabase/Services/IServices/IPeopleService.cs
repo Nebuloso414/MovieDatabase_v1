1947dbe2ecce675ea3efdd89202062e758f44ea5
Guillermo Garcia <g2beastie@live.com>
2025-06-08 14:33:01 -0400

    Added People Service

---
+++ MovieDatabase/Services/IServices/IPeopleService.cs
@@ -0,0 +1,16 @@
+﻿using MovieDatabase.Models;
+using MovieDatabase.Models.Dto;
+using System.Linq.Expressions;
+
+namespace MovieDatabase.Services.IServices
+{
+    public interface IPeopleService
+    {
+        Task<IEnumerable<People>> GetAllAsync(Expression<Func<People, bool>>? filter = null, string? includeProperties = null);
+        Task<People?> GetByIdAsync(Expression<Func<People, bool>>? filter = null, bool tracked = true, string? includeProperties = null);
+        Task<IEnumerable<People?>> GetByNameAsync(string name);
+        Task<People> CreateAsync(PeopleCreateDto peopleDto);
+        Task DeleteAsync(People people);
+        Task<People> UpdateAsync(PeopleUpdateDto updatedPeople);
+    }
+}
---
+++ MovieDatabase/Services/PeopleService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MovieDatabase.Models;
+using MovieDatabase.Models.Dto;
+using MovieDatabase.Repository.IRepository;
+using MovieDatabase.Services.IServices;
+using System.Linq.Expressions;
+
+namespace MovieDatabase.Services
+{
+    public class PeopleService : IPeopleService
+    {
+        private readonly IPeopleRepository _peopleRepository;
+        private readonly IMapper _mapper;
+
+        public PeopleService(IPeopleRepository peopleRepository, IMapper mapper)
+        {
+            _peopleRepository = peopleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<People>> GetAllAsync(Expression<Func<People, bool>>? filter = null, string? includeProperties = null)
+        {
+            return await _peopleRepository.GetAllAsync(filter, includeProperties);
+        }
+
+        public async Task<People?> GetByIdAsync(Expression<Func<People, bool>>? filter = null, bool tracked = true, string? includeProperties = null)
+        {
+            return await _peopleRepository.GetByIdAsync(filter, tracked, includeProperties);
+        }
+
+        public async Task<IEnumerable<People?>> GetByNameAsync(string name)
+        {
+            return await _peopleRepository.GetByNameAsync(name);
+        }
+        public async Task<People> CreateAsync(PeopleCreateDto peopleDto)
+        {
+            var people = _mapper.Map<People>(peopleDto);
+            await _peopleRepository.CreateAsync(people);
+            return people;
+        }
+
+        public async Task DeleteAsync(People people)
+        {
+            await _peopleRepository.RemoveAsync(people);
+        }
+
+        public async Task<People> UpdateAsync(PeopleUpdateDto updatedPeople)
+        {
+            var people = _mapper.Map<People>(updatedPeople);
+            await _peopleRepository.UpdateAsync(people);
+            return people;
+        }
+    }
+}
