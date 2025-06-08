b80e65de4ba13a14c47a2cdd83517b73dd3a1bd9
Guillermo Garcia <g2beastie@live.com>
2025-06-08 14:37:14 -0400

    Added PeopleDto

---
+++ MovieDatabase/Models/Dto/PeopleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieDatabase.Models.Dto
+{
+    public class PeopleDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
